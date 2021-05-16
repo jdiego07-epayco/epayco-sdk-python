af6aa8b597c4e1a9f1b19a1feb2222d5d5a79388
epayco-jdiego07 <juan.vargas.epayco@gmail.com>
2021-05-15 19:39:30 -0500

    Actualización de tipo de dato en variable split_receivers

--- split.py
+++ split.py
@@ -11,29 +11,31 @@ options = {"apiKey": apiKey, "privateKey": privateKey,
 
 objepayco = epayco.Epayco(options)
 
-credit_info = {
-    "card[number]": "5240521756556621",
-    "card[exp_year]": "2027",
-    "card[exp_month]": "02",
-    "card[cvc]": "049"
-}
+# credit_info = {
+#     "card[number]": "5240521756556621",
+#     "card[exp_year]": "2027",
+#     "card[exp_month]": "02",
+#     "card[cvc]": "049"
+# }
 
-token = objepayco.token.create(credit_info)
+# token = objepayco.token.create(credit_info)
 
-customer_info = {
-    "token_card": token['id'],
-    "name": "Juan Diego",
-    "last_name": "Vargas Posada",  # This parameter is optional
-    "email": "diego.vargas@payco.co",
-    "phone": "3042462218",
-    "default": False
-}
+# customer_info = {
+#     "token_card": token['id'],
+#     "name": "Juan Diego",
+#     "last_name": "Vargas Posada",  # This parameter is optional
+#     "email": "diego.vargas@payco.co",
+#     "phone": "3042462218",
+#     "default": False
+# }
 
-customer = objepayco.customer.create(customer_info)
+# customer = objepayco.customer.create(customer_info)
 
 payment_info = {
-    "token_card": token['id'],
-    "customer_id": customer['data']['customerId'],
+    # "token_card": token['id'],
+    # "customer_id": customer['data']['customerId'],
+    "token_card": "0837e22f054270493229d03",
+    "customer_id": "0754200c0e6dd165e4c5344",
     "doc_type": "CC",
     "doc_number": "1194418306",
     "name": "Juan Diego",
@@ -41,9 +43,9 @@ payment_info = {
     "email": "diego.vargas@payco.co",
     "bill": str(random.randint(100000, 999999)),
     "description": "SDK PYTHON Pruebas ePayco Split TC",
-    "value": "6",
+    "value": "1",
     "tax": "0",
-    "tax_base": "6",
+    "tax_base": "1",
     "currency": "COP",
     "dues": "1",
     "ip": "181.134.247.50",  # This is the client's IP, it is required
@@ -57,9 +59,12 @@ payment_info = {
     "split_primary_receiver": "515360",
     "split_primary_receiver_fee": "0",
     "split_rule": 'multiple',
-    "split_receivers": str(json.dumps([
-        {"id": "9898", "total": "3","iva": "0", "base_iva": "3", "fee": "1"}
-    ])),
+    "split_receivers": [
+        {"id": "9898", "total": "1","iva": "0", "base_iva": "1", "fee": "1"} # 1- 1
+    ],
+    # "split_receivers": str(json.dumps([
+    #     {"id": "9898", "total": "3","iva": "0", "base_iva": "3", "fee": "1"} # Multiple
+    # ])),
     "use_default_card_customer": False,
     "extra1": "",
     "extra2": "",
